f45eb15e8b1e5483202b10718ff64499945f9854
emersonasdf <equeiroz.sp+github@gmail.com>
2021-03-14 19:43:20 -0300

    Code cleanup

--- src/main/kotlin/com/n26/web/GlobalControllerExceptionHandler.kt
+++ src/main/kotlin/com/n26/web/GlobalControllerExceptionHandler.kt
@@ -5,13 +5,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(InvalidFormatException::class)
-    fun validationErrorHandler(req: HttpServletRequest?, e: Exception): ResponseEntity<Any> {
+    fun validationErrorHandler(): ResponseEntity<Any> {
         return ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY)
     }
 }
